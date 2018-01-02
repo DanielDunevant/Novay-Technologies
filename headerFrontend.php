f4823ab51ae3318595ca88e3dd704bcdd3a6b428
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-01-02 16:25:11 -0600

    Centering Options

--- headerFrontend.php
+++ headerFrontend.php
@@ -1,28 +1,11 @@
-<!-- PHP for "Novay Technologies" © 2017, authors: "Daniel Dunevant & Jesse Primiani" -->
+<!-E PHP for "Novay Technologies" © 2017, authors: "Daniel Dunevant & Jesse Primiani" -->
 <?php
 echo<<<HEREDOC
 <div class ="row">
-	<a class="col-xs-12 col-md-12 col-sm-12"  href="index.php?page=start" >
+	<a class="col-xs-10 col-md-10 col-sm-10"  href="index.php?page=start" >
 		<img class = "img-responsive" id ='logoLarge' src ='images/NovayLogoVector.svg'>
 	</a>
-</div>
 HEREDOC;
-$linkArray = array("start",
-		   "about",
-		   "services",
-		   "contact");
-$textArray = array("Home","About","Services","Contact");
-$numLinks = 4;
-print "<div id ='headerOptions' class='row'>";
-for($i = 0; $i <$numLinks;$i++)
-{
-	print "	<a class='headerLinks'  href='index.php?page=$linkArray[$i]' >";
-	print "<div class = 'link-boxes col-xs-12 col-md-2 col-sm-2'>";
-	print "$textArray[$i]	";
-	print "</div>";
-	print "</a>";
-}
-
 $socialLinkArray = array ("https://www.linkedin.com/company/novay-technologies/"
 			  ,"https://www.facebook.com/Novay-Technologies-390582464615610/");
 $socialImageArray = array ("linkedin_Icon.png","Facebook-Logo.png");
@@ -37,6 +20,24 @@ for ($i = 0 ;$i <$numSocial ;$i++)
 }
 print "</div>";
 //images/"image name"
+$linkArray = array("start",
+		   "about",
+		   "services",
+		   "contact");
+$textArray = array("Home","About","Services","Contact");
+$numLinks = 4;
+print "<div id ='headerOptions' class='row'>";
+print "<div id = 'headerOptionsInner' >";
+for($i = 0; $i <$numLinks;$i++)
+{
+	print "	<a class='headerLinks'  href='index.php?page=$linkArray[$i]' >";
+	print "<div class = 'link-boxes col-xs-12 col-md-2 col-sm-2'>";
+	print "$textArray[$i]	";
+	print "</div>";
+	print "</a>";
+}
+print "</div>";
+print "</div>";
 
 
 ?>
