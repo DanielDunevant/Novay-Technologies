a2a4bff847bab54aa874e0e275b8d2104b2972b6
DanielDunevant <daniel.john.dunevant@gmail.com>
2018-01-05 11:15:25 -0600

    Various Header and Social Bar Updates

--- headerFrontend.php
+++ headerFrontend.php
@@ -2,22 +2,10 @@
 <?php
 echo<<<HEREDOC
 <div class ="row">
-	<a class="col-xs-10 col-md-10 col-sm-10"  href="index.php?page=start" >
+	<a class="col-xs-12 col-md-12 col-sm-12"  href="index.php?page=start" >
 		<img class = "img-responsive" id ='logoLarge' src ='images/NovayLogoVector.svg'>
 	</a>
 HEREDOC;
-$socialLinkArray = array ("https://www.linkedin.com/company/novay-technologies/"
-			  ,"https://www.facebook.com/Novay-Technologies-390582464615610/");
-$socialImageArray = array ("linkedin_Icon.png","Facebook-Logo.png");
-$numSocial = 2;
-for ($i = 0 ;$i <$numSocial ;$i++)
-{
-	print "<div class = 'social-boxes col-xs-1 col-md-1 col-sm-1'>";
-	print "<a class = 'socialLinks' href ='$socialLinkArray[$i]' target = '_blank'>";	
-	print "<img class = 'socialImages' src = 'images/$socialImageArray[$i]'>";	
-	print "</a>";	
-	print "</div>";
-}
 print "</div>";
 //images/"image name"
 $linkArray = array("start",
@@ -27,6 +15,8 @@ $linkArray = array("start",
 $textArray = array("Home","About","Services","Contact");
 $numLinks =4;
 print "<div id ='headerOptions' class='row'>";
+print "<div class = 'link-boxes col-xs-12 col-md-2 col-sm-2'>";
+print "</div>";
 for($i = 0; $i <$numLinks;$i++)
 {
 	print "<a class='headerLinks'  href='index.php?page=$linkArray[$i]' >";
--- main.php
+++ main.php
@@ -3,7 +3,7 @@
 print"<!-- PHP for 'Novay Technologies' © 2017, authors: 'Daniel Dunevant & Jesse Primiani' -->";
 $pageValue = $_GET['page'];
 $arrayOfPages = array("start","about","services","contact");
-if (in_array($pageValue,$arrayOfPages) )
+if (in_array($pageValue,$arrayOfPages)) 
 {
 	print "<div class='row'>\n";
 	print "<div class = 'col-md-11 col-sm-11 col-xs-11'>\n";
@@ -18,12 +18,11 @@ else
 {
 	print "<div class='row'>\n";
 	print "<div class = 'col-md-11 col-sm-11 col-xs-11'>\n";
-        require_once( $pageValue.".php");
+        require_once( "start.php");
 	print "</div>\n";
 	print "<div class = 'col-md-1 col-sm-1 col-xs-1'>\n";
 	require "socialPanel.php";
 	print "</div>\n";
 	print "</div>\n";
 }
-
 ?>
