0d56f10cffde30cb882521633d6e3e6d69a9d7e9
Daniel John Dunevant <ddunevant@ivytech.edu>
2018-01-08 12:38:39 -0600

    Header Change(Style change Coming later tonight)

--- headerFrontend.php
+++ headerFrontend.php
@@ -1,6 +1,19 @@
-
-<!-E PHP for "Novay Technologies" © 2017, authors: "Daniel Dunevant & Jesse Primiani" -->
+<!-- PHP for "Novay Technologies" © 2017, authors: "Daniel Dunevant & Jesse Primiani" -->
 <?php
+$linkArray = array("start",
+                   "about",
+                   "services",
+                   "contact");
+$textArray = array("Home","About","Services","Contact");
+$numLinks =4;
+print "<div id ='headerOptions' class='row'>\n";
+print "<img src ='images/NovayHeader_Dark.svg'   class = 'col-xs-2 col-md-2 col-sm-2'  >\n";
+for($i = 0; $i <$numLinks;$i++){
+        print "<a class='headerLinks col-xs-2 col-md-2 col-sm-2'  href='index.php?page=$linkArray[$i]'\n >";
+        print "$textArray[$i]";
+        print "</a>\n";
+}
+print "</div>\n";
 echo<<<HEREDOC
 <div class ="row">
         <a class="col-xs-12 col-md-12 col-sm-12"  href="index.php?page=start" >
@@ -8,33 +21,19 @@ echo<<<HEREDOC
         </a>
 HEREDOC;
 print "</div>";
-//images/"image name"
-$linkArray = array("start",
-                   "about",
-                   "services",
-                   "contact");
-$textArray = array("Home","About","Services","Contact");
-$numLinks =4;
-print "<div id ='headerOptions' class='row'>";
-print "<img src ='images/NovayHeader_Dark.svg' class = 'img-responsive'>";
+
+//Alternate Header Style
+/*
+print "<div id ='headerOptions2' class='row'>\n";
+print "<img src ='images/NovayHeader_Light.svg'   class = ' col-xs-2 col-md-2 col-sm-2' >\n";
 for($i = 0; $i <$numLinks;$i++)
 {
-        print "<a class='headerLinks col-xs-12 col-md-2 col-sm-2'  href='index.php?page=$linkArray[$i]' >";
+        print "<a class='headerLinks2 col-xs-2 col-md-2 col-sm-2'  href='index.php?page=$linkArray[$i]' >\n";
         print "$textArray[$i]";
-        print "</a>";
+        print "</a>\n";
 }
-print "</div>";
-
-print "<div id ='headerOptions2' class='row'>";
-print "<img src ='images/NovayHeader_Dark.svg' class = 'img-responsive'>";
-for($i = 0; $i <$numLinks;$i++)
-{
-        print "<a class='headerLinks col-xs-12 col-md-2 col-sm-2'  href='index.php?page=$linkArray[$i]' >";
-        print "$textArray[$i]";
-        print "</a>";
-}
-print "</div>";
-
+print "</div>\n";
+*/
 
 ?>
 
