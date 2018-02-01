7b209c2f2a8819005b812a508b4eb84d039f2bf7
Jesse T Primiani <jesseprimiani@gmail.com>
2018-02-01 03:51:54 -0600

    added temporary lock

---
+++ lock.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+	<meta charset="UTF-8" />
+	<meta name="author" content="Novay Technologies" />
+	<title>Novay Technologies</title>
+	<link rel="shortcut icon" href="/favicon.ico" />
+	<link rel="icon" type="image/png" href="/favicon.png">
+</head>
+<body>
+	<h1 style="text-align:center;">Novay Technologies</h1>
+	<h2 style="text-align:center;">This site is currently under development</h2>
+	<p style="text-align:center;">If you need to contact us, feel free to do so at: support@novaytechnologies.com</p>
+</body>
+</html>
